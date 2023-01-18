0d9031314fdd227ccd42ca7bbed3cd6902ebda22
barbararech <rech.brb@gmail.com>
2023-01-18 13:49:47 -0300

    feat: add user model and user repository

--- src/main/java/com/tweteroo/api/repository/UserRepository.java
+++ src/main/java/com/tweteroo/api/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.tweteroo.api.repository;
 
-public class UserRepository {
-  
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tweteroo.api.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
